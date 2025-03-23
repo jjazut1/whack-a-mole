e070d883fe20baac2ac04cc3e1ff5f0b40c9f130
jjazut1 <jjazut1@gmail.com>
2025-03-23 07:36:02 -0600

    v72

--- script.js
+++ script.js
@@ -172,8 +172,8 @@ function setupHolesAndMoles() {
     // Increasing Z values moves the holes further down/away from the camera
     const holes = [
         { x: -1.5, z: -1.5, rotation: Math.PI * 0.25 + 0.175, description: "back Left" },     // Moved up by decreasing Z
-        { x: 2, z: -1.5, rotation: -Math.PI * 0.25 - 0.175, description: "Front Right" },     // Moved up by decreasing Z
-        { x: -2, z: 1.5, rotation: Math.PI * 0.75 + 0.175, description: "front Left" },       // Moved toward center by decreasing Z
+        { x: 3, z: -1.5, rotation: -Math.PI * 0.25 - 0.175, description: "Front Right" },     // Moved up by decreasing Z
+        { x: -2.3, z: 1.5, rotation: Math.PI * 0.75 + 0.175, description: "front Left" },       // Moved toward center by decreasing Z
         { x: 2, z: 2.0, rotation: -Math.PI * 0.75 - 0.175, description: "Back Right" }        // Moved toward center by decreasing Z
     ];
 
